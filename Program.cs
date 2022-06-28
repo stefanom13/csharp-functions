6a28aa534ebb40fc17689b8e7c233f45d92dbcf0
stefanom13 <stefanomancuso13@gmail.com>
2022-06-28 14:26:33 +0200

    funzioni di base create

--- Program.cs
+++ Program.cs
@@ -4,7 +4,53 @@
     {
         static void Main(string[] args)
         {
-            
+            void StampaArray(int[] array)
+            {
+                Console.Write("[");
+
+                for (int i = 0; i < array.Length; i++)
+                {
+                    Console.Write(array[i]);
+                    if (i < array.Length - 1)
+                    {
+                        Console.Write(", ");
+                    }
+                }
+
+
+                Console.WriteLine("]");
+
+            }
+            int Quadrato(int num)
+            {
+                return num * num;
+            }
+
+
+            int[] ElevaArrayAlQuadrato(int[] array)
+            {
+                int[] arrayQuadrato = new int[array.Length];
+
+                for (int i = 0; i < array.Length; i++)
+                {
+                    arrayQuadrato[i] = Quadrato(array[i]);
+                }
+
+                return arrayQuadrato;
+            }
+
+
+            int sommaElementiArray(int[] array)
+            {
+                int somma = 0;
+
+                for (int i = 0; i < array.Length; i++)
+                {
+                    somma += array[i];
+                }
+
+                return somma;
+            }
         }
     }
 }
